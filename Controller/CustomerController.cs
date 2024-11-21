acbe0615d1c0910a6045055d4b47817e550a3dd8
rishabh.911996 <rishabh.911996@gmail.com>
2024-11-20 21:23:48 -0700

    Working apis for other assignment operations.

--- Controller/CustomerController.cs
+++ Controller/CustomerController.cs
@@ -28,22 +28,69 @@ namespace BAIS3150_WEBAPI_LAB5.Controller
             return customer;
         }
 
-        // POST api/<CustomerController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        // GET api/<CustomerController>/IsPrime/{number}
+        [HttpGet("IsPrime/{number}")]
+        public bool IsPrime(int number)
         {
+            if (number <= 1) return false;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
         }
 
-        // PUT api/<CustomerController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        // GET api/<CustomerController>/BinaryToDecimal/{binary}
+        [HttpGet("BinaryToDecimal/{binary}")]
+        public int BinaryToDecimal(string binary)
         {
+            int decimalValue = 0;
+            int baseValue = 1;
+
+            for (int i = binary.Length - 1; i >= 0; i--)
+            {
+                if (binary[i] == '1')
+                {
+                    decimalValue += baseValue;
+                }
+                baseValue *= 2;
+            }
+
+            return decimalValue;
         }
 
-        // DELETE api/<CustomerController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // GET api/<CustomerController>/DecimalToBinary/{decimalNumber}
+        [HttpGet("DecimalToBinary/{decimalNumber}")]
+        public string DecimalToBinary(int decimalNumber)
         {
+            if (decimalNumber == 0) return "0";
+
+            string binary = string.Empty;
+            while (decimalNumber > 0)
+            {
+                binary = (decimalNumber % 2) + binary;
+                decimalNumber /= 2;
+            }
+
+            return binary;
         }
+
+        //// POST api/<CustomerController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<CustomerController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<CustomerController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
